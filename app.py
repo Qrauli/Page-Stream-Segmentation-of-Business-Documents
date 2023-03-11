6118a098c805a1e6472b67e13692841c5228423a
Qrauli <lucas.gugler@gmail.com>
2023-03-11 13:03:50 +0100

    GitHub Actions Setup with temporary test file [#12]

--- app.py
+++ app.py
@@ -1,3 +1,7 @@
 from fastapi import FastAPI
 
 app = FastAPI(title="API", description="API for PSS", version="1.0")
+
+@app.get('/predict', tags=["predictions"])
+async def get_prediction():
+    return {"prediction": "test"}
