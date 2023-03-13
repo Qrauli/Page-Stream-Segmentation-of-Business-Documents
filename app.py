87f6eefa11a26ddd5f74c8fc15f0faa87e735c08
Qrauli <lucas.gugler@gmail.com>
2023-03-13 13:20:55 +0100

    uploaded files get split into single pages and added to zip file, predict function has to be changed to model prediction [#9]

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from fastapi import FastAPI, File, UploadFile, HTTPException
 from fastapi.responses import StreamingResponse
 import io
 import zipfile
+from PyPDF2 import PdfReader, PdfWriter
 
 tags_metadata = [
     {
@@ -10,8 +11,8 @@ tags_metadata = [
     }
 ]
 
-
 app = FastAPI(title="API", description="API for PSS", version="1.0", openapi_tags=tags_metadata)
+
 
 @app.post('/predict', tags=["prediction"])
 async def get_prediction(file: UploadFile):
@@ -19,11 +20,35 @@ async def get_prediction(file: UploadFile):
         raise HTTPException(400, detail="Invalid document type")
 
     pdf_content = await file.read()
+    splits = await predict(pdf_content)
     zip_stream = io.BytesIO()
 
     with zipfile.ZipFile(zip_stream, mode="w") as zip_file:
-        # Add the PDF file to the zip file
-        zip_file.writestr(file.filename, pdf_content)
+        pdf_reader = PdfReader(io.BytesIO(pdf_content))
+        pdf_writer = PdfWriter()
+        start = 1
+        end = len(splits)
+        for i in range(len(splits)):
+            if splits[i] == 1:
+                pdf_writer = PdfWriter()
+                start = i+1
+
+            # Add the current page to the PdfFileWriter object
+            pdf_writer.add_page(pdf_reader.pages[i])
+
+            if i == len(splits) - 1 or splits[i+1] == 1:
+                end = i+1
+                # Create an in-memory byte stream to hold the new PDF file contents
+                pdf_stream = io.BytesIO()
+
+                # Write the new PDF file to the in-memory byte stream
+                pdf_writer.write(pdf_stream)
+
+                # Set the position of the in-memory byte stream to the beginning
+                pdf_stream.seek(0)
+
+                # Add the new PDF file to the zip file
+                zip_file.writestr(f"page_{start}-{end}.pdf", pdf_stream.getvalue())
 
     zip_stream.seek(0)
 
@@ -34,3 +59,13 @@ async def get_prediction(file: UploadFile):
     response.headers["Content-Disposition"] = f"attachment; filename={file.filename}.zip"
 
     return response
+
+
+async def predict(pdf_content: bytes):
+    # temporary split calculation
+    pdf_reader = PdfReader(io.BytesIO(pdf_content))
+    count = len(pdf_reader.pages)
+    categories = []
+    for i in range(count):
+        categories.append(1)
+    return categories
