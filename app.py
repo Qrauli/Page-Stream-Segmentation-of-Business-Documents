b08377204e54dd306d26cb515a4698714debbfbe
Qrauli <lucas.gugler@gmail.com>
2023-03-27 13:03:18 +0200

    added model loading and pdf conversion to images [#9]

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI, File, UploadFile, HTTPException
+from fastapi import FastAPI, UploadFile, HTTPException
 from fastapi.responses import StreamingResponse
 import io
 import zipfile
@@ -7,6 +7,12 @@ from contextlib import asynccontextmanager
 from tensorflow import keras
 from pdf2image import convert_from_bytes
 import pytesseract
+from PIL import Image, ImageEnhance, ImageFilter
+from models.training import image_to_tensor, tokenize_from_ocr
+import tensorflow as tf
+import numpy as np
+from transformers import TFLayoutLMModel, LayoutLMTokenizer
+from huggingface_hub import from_pretrained_keras
 
 tags_metadata = [
     {
@@ -15,22 +21,27 @@ tags_metadata = [
     }
 ]
 
-
-model = []
+model = None
+tokenizer = None
 
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
     # Load the ML model
     global model
-    # model = keras.models.load_model('path/to/location')
-    model = "t"
+    global tokenizer
+    model = from_pretrained_keras("qrauli/pss-business-documents", custom_objects={"TFLayoutLMModel":
+    TFLayoutLMModel})
+    # model = "t"
+    tokenizer = LayoutLMTokenizer.from_pretrained("microsoft/layoutlm-base-uncased")
     yield
     # Clean up the ML models and release the resources
-    # keras.backend.clear_session()
-    model = "r"
+    keras.backend.clear_session()
+    # model = "r"
+
 
 app = FastAPI(title="API", description="API for PSS", version="1.0", openapi_tags=tags_metadata, lifespan=lifespan)
+
 
 @app.post('/predict', tags=["prediction"])
 async def get_prediction(file: UploadFile):
@@ -49,13 +60,13 @@ async def get_prediction(file: UploadFile):
         for i in range(len(splits)):
             if splits[i] == 1:
                 pdf_writer = PdfWriter()
-                start = i+1
+                start = i + 1
 
             # Add the current page to the PdfFileWriter object
             pdf_writer.add_page(pdf_reader.pages[i])
 
-            if i == len(splits) - 1 or splits[i+1] == 1:
-                end = i+1
+            if i == len(splits) - 1 or splits[i + 1] == 1:
+                end = i + 1
                 # Create an in-memory byte stream to hold the new PDF file contents
                 pdf_stream = io.BytesIO()
 
@@ -80,7 +91,38 @@ async def get_prediction(file: UploadFile):
 
 
 async def predict(pdf_content: bytes):
+    global tokenizer
+    global model
+    images = convert_from_bytes(pdf_content)
 
+    # convert each image to input data
+    input_data = []
+    for image in images:
+        pil_image = image
+        # image_tensor = tf.convert_to_tensor(pil_image)
+        image_tensor = image_to_tensor(tf.convert_to_tensor(pil_image), (224, 224))
+        pil_image = pil_image.filter(ImageFilter.MedianFilter())
+        pil_image = pil_image.resize((1000, 1000))
+        enhancer = ImageEnhance.Contrast(pil_image)
+        pil_image = enhancer.enhance(2)
+        pil_image = pil_image.convert('1')
+        dataframe = pytesseract.image_to_data(pil_image, output_type=pytesseract.Output.DATAFRAME)
+        input_ids, bbox, attention_mask, token_type_ids = tokenize_from_ocr("", tokenizer, 100, dataframe)
+        # print(dataframe)
+        input_data.append(
+            {"image": image_tensor, "input_ids": input_ids, "bbox": bbox, "attention_mask": attention_mask,
+             "token_type_ids": token_type_ids})
+        # pil_images.append(pil_image)
+
+    batch_generator = tf.data.Dataset.from_generator(lambda: generator(input_data),
+                                                     output_types=({"input_ids": tf.int32, 'bbox': tf.int32,
+                                                                    'attention_mask': tf.int32,
+                                                                    "token_type_ids": tf.int32}, tf.int64, tf.int64,
+                                                                   tf.string))
+    input_ids, bbox, attention_mask, token_type_ids, image = batch_generator.batch(len(input_data)).get_single_element()
+    classification = model([input_ids, bbox, attention_mask, token_type_ids, image])
+    # classification[0] = 1
+    print(classification)
     # temporary split calculation
     pdf_reader = PdfReader(io.BytesIO(pdf_content))
     count = len(pdf_reader.pages)
@@ -88,3 +130,16 @@ async def predict(pdf_content: bytes):
     for i in range(count):
         categories.append(1)
     return categories
+
+
+def generator(input_data):
+    i = 0
+    while i < len(input_data):
+        input_ids = tf.convert_to_tensor(np.array(input_data[i].input_ids))
+        bbox = tf.convert_to_tensor(np.array(input_data[i].bbox))
+        attention_mask = tf.convert_to_tensor(np.array(input_data[i].attention_mask))
+        token_type_ids = tf.convert_to_tensor(np.array(input_data[i].token_type_ids))
+        image = tf.convert_to_tensor(np.array(input_data[i].image))
+
+        yield input_ids, bbox, attention_mask, token_type_ids, image
+        i += 1
